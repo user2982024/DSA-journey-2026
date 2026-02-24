5ee7557097c8ae3c195a036ac6f1a18e661f042d
user2982024 <sheikhabrar217@gmail.com>
2026-02-24 07:26:21 +0500

    Running sum of 1D Array optimized!

--- Leetcode/Arrays/Running_Sum_of_1D_Array.cpp
+++ Leetcode/Arrays/Running_Sum_of_1D_Array.cpp
@@ -106,6 +106,22 @@ public:
     }
 };
 
+/*
+        ***      More Optimized      ***   
+        class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 1; i < n; i ++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+};
+*/
+
 
 /***************************************************************
  * Optional Testing (Local Testing Only)
